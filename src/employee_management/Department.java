23efb7b14e97c519af81302a1879154748f05a9e
Fiantso Harena <fiantsorav@gmail.com>
2023-07-29 12:00:47 +0300

    fix: Fixing the Fifth issue

--- src/employee_management/Department.java
+++ src/employee_management/Department.java
@@ -1,5 +1,8 @@
 package employee_management;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * TODO : a department is a collection of employees. It has a name and a set of employees in it.
  */
@@ -12,4 +15,31 @@ public class Department {
      * TODO : for our HR officer, give a function to show all details about employees, including
      * their pay.
      */
+    private String name;
+    private Set<Employee> employees;
+
+    public Department(String name) {
+        this.name = name;
+        this.employees = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+    }
+
+    public void removeEmployee(Employee employee) {
+        employees.remove(employee);
+    }
 }
